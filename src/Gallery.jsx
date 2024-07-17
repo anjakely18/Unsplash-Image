5680f606e9d225a2501e20e6f65330fcbda6fab8
anjakely18 <anjaniaina03@gmail.com>
2024-07-17 22:23:20 +0200

    Setup query

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,6 +1,19 @@
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const url =
+  "https://api.unsplash.com/search/photos?client_id=MxLJbpyGHF3N5aQsegq9Pb3kwJ4Ks6Js2JVu4EAokYg&query=office";
 
 const Gallery = () => {
+  const response = useQuery({
+    queryKey: ["images"],
+    queryFn: async () => {
+      const result = await axios.get(url);
+      return result.data;
+    },
+  });
+  console.log(response);
   return <div className="image-container"></div>;
 };
 
